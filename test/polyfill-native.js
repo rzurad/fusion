63787883898aec988e3da9567fb2c09f4a8799ca
Richard Zurad <rzurad@gmail.com>
2012-07-19 00:38:21 -0700

    stubbed failing test case for making sure fusion does not touch native objects when POLYFILL_NATIVE is false

--- test/polyfill-native.js
+++ test/polyfill-native.js
@@ -4,27 +4,28 @@
     var global = this,
         fusion = global.fusion || require('fusion'),
         buster = global.buster || require('buster'),
+        forEach = fusion.array.forEach,
+        keys = fusion.object.keys,
 
         _expected = global.POLYFILL_NATIVE_EXPECTED;
 
 
     buster.testCase('does fusion behave when POLYFILL_NATIVE is false?', {
         test: function () {
-            var k, pk;
-
             if (!_expected) {
                 assert(false, 'CONFIG not loaded');
             }
 
-            forEach(object.keys(_expected), function (key) {
+            //TODO: needs more recursion
+            forEach(keys(_expected), function (k) {
                 if (_expected[k] === 'prototype') {
-                    for (pk in object.keys(_expected.prototype])) {
+                    forEach(keys(_expected.prototype), function (pk) {
                         assert.same(_expected[k][pk], global[k][pk]);
-                    }
+                    });
                 } else {
                     assert.same(_expected[k], global[k]);
                 }
-            }
+            });
         }
     });
 }).call(this);
